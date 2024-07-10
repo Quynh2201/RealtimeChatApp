72bdd2e45070f2ed80db54b77e0cb8e66d8033c6
Quynh2201 <21012322@st.phenikaa-uni.edu.vn>
2024-07-10 23:42:13 +0700

    FE for the whole application

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,9 +1,206 @@
-import { usePage } from "@inertiajs/react"
+import TextInput from "@/Components/TextInput";
+import { router, usePage } from "@inertiajs/react"
+import { useEffect, useState } from "react";
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
+import ConversationItem from "../Components/App/ConversationItem"
+import { useEventBus } from "@/EventBus";
+import GroupModal from "@/Components/App/GroupModal";
 
-const ChatLayout = () => {
+const ChatLayout = ({ children }) => {
+    const page = usePage();
+    const currentUser = page.props.auth.user;
+    const conversations = page.props.auth.conversations;
+    const selectedConversation = page.props.auth.selectedConversation;
+    const [onlineUsers, setOnlineUsers] = useState({});
+    const [localConversations, setLocalConversations] = useState([]);
+    const [sortedConversations, setSortedConversations] = useState([]);
+    const [showGroupModal, setShowGroupModal] = useState(false);
+    const { on, emit } = useEventBus();
+
+    const isUserOnline = (userId) => onlineUsers[userId];
+
+    const onSearch = (e) => {
+        const search = e.target.value.toLowerCase();
+        setLocalConversations(
+            conversations.filter((conversation) => {
+                return conversation.name.toLowerCase().includes(search);
+            })
+        );
+    }
+
+    const messageCreated = (message) => {
+        setLocalConversations((oldConversations) => {
+            return oldConversations.map((u) => {
+                if(message.receiver_id && !u.is_group && (u.id == message.sender_id || u.id == message.receiver_id) && u.id != currentUser.id) {
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+                if(message.group_id && u.is_group && u.id == message.group_id){
+                    u.last_message = message.message;
+                    u.last_message_date = message.created_at;
+                    return u;
+                }
+                return u;
+            });
+        });
+    };
+
+    const messageDeleted = ({ prevMessage }) => {
+        if(!prevMessage) {
+            return;
+        }
+        
+        // Find conversation by lastMessage & update last_message
+        messageCreated(prevMessage);
+    };
+
+    // Create channel to recognize online users
+    useEffect(() => {
+        Echo.join('online')
+            .here((users) => {
+                // console.log("Here", users);
+                const onlineUsersObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+
+                setOnlineUsers((prevOnlineUsers) => {
+                    return { ...prevOnlineUsers, ...onlineUsersObj };
+                });
+            })
+            .joining((user) => {
+                // console.log("Joining", user);
+                setOnlineUsers((prevUsers) => {
+                    const updatedUsers = { ...prevUsers };
+                    updatedUsers[user.id] = user;
+                    return updatedUsers;
+                })
+            })
+            .leaving((user) => {
+                // console.log("Leaving", user);
+                setOnlineUsers((prevUsers) => {
+                    const updatedUsers = { ...prevUsers };
+                    delete updatedUsers[user.id];
+                    return updatedUsers;
+                })
+            })
+            .error((error) => {
+                console.log("Error", error);
+            });
+
+        return () => {
+            Echo.leave('online');
+        }
+    }, []);
+
+    useEffect(() => {
+        setLocalConversations(conversations);
+    }, [conversations])
+
+    useEffect(() => {
+        setSortedConversations(
+            localConversations.sort((a, b) => {
+                if (a.blocked_at && b.blocked_at)
+                    return a.blocked_at > b.blocked_at ? 1 : -1;
+                else if (a.blocked_at)
+                    return 1;
+                else if (b.blocked_at)
+                    return -1;
+
+                if (a.last_message_date && b.last_message_date)
+                    return b.last_message_date.localeCompare(a.last_message_date);
+                else if (a.last_message_date) 
+                    return -1;
+                else if (b.last_message_date)
+                    return 1;
+                else
+                    return 0;
+            })
+        );
+    }, [localConversations])
+
+    useEffect(() => {
+        const offCreated = on("message.created", messageCreated);
+        const offDeleted = on("message.deleted", messageDeleted);
+        const offModalShow = on("GroupModal.show", (group) => {
+            setShowGroupModal(true);
+        })
+        const offGroupDelete = on("group.deleted", ({ id, name }) => {
+            setLocalConversations((oldConversations) => {
+                return oldConversations.filter((conversation) => conversation.id != id);
+            })
+
+            emit('toast.show', `Group ${name} was deleted`);
+
+            if(!selectedConversation || (selectedConversation && selectedConversation.is_group && selectedConversation.id == id)) {
+                router.visit(route("dashboard"));
+            }
+        });
+
+        return () => {
+            offCreated();
+            offDeleted();
+            offModalShow();
+            offGroupDelete();
+        };
+    }, [on]);
+
     return (
-        <></>
-    )
+        <>
+            <div className="flex-1 w-full flex overflow-hidden">
+                <div
+                    className={`transition-all hidden sm:w-[220px] sm:flex sm:flex-col md:w-[300px] bg-slate-800 overflow-hidden ${
+                        selectedConversation ? "-ml-[100%] sm:ml-0" : ""
+                    }`}
+                >
+                    {/* Title */}
+                    <div className="flex items-center justify-between py-2 px-3 text-xl font-medium text-gray-200">
+                        My Conversations
+                        <div
+                            className="tooltip tooltip-left"
+                            data-tip="Create new group"
+                        >
+                            <button
+                            onClick={e => setShowGroupModal(true)}
+                                className="text-gray-400 hover:text-gray-200"
+                            >
+                                <PencilSquareIcon className="w-4 h-4 inline-block ml-2" />
+                            </button>
+                        </div>
+                    </div>
+
+                    {/*  */}
+                    <div className="px-3 pb-3">
+                        <TextInput
+                            onKeyUp={onSearch}
+                            placeholder="Filter users and groups"
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="flex-1 overflow-auto">
+                        {sortedConversations && 
+                            sortedConversations.map((conversation) => (
+                                <ConversationItem 
+                                    key={`${
+                                        conversation.is_group ? 
+                                            "group_"
+                                            : "user_"
+                                    }${conversation.id}`}
+                                    conversation={conversation}
+                                    online={!!isUserOnline(conversation.id)}
+                                    selectedConversation={selectedConversation}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    {children}
+                </div>
+            </div>
+            <GroupModal show={showGroupModal} onClose={() => setShowGroupModal(false)} />
+        </>
+    );
 }
 
-export default ChatLayout
+export default ChatLayout;
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,21 +1,205 @@
+import ChatLayout from '@/Layouts/ChatLayout';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import ConversationHeader from '@/Components/App/ConversationHeader';
+import MessageInput from '@/Components/App/MessageInput';
+import MessageItem from '@/Components/App/MessageItem';
+import AttachmentPreviewModal from '@/Components/App/AttachmentPreviewModal';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
+import { useEventBus } from '@/EventBus';
+import axios from 'axios';
 
-export default function Home({ auth }) {
+const Home = ({ selectedConversation = null, messages = null }) => {
+
+    const [localMessages, setLocalMessages] = useState([]);
+    const [noMoreMessages, setNoMoreMessages] = useState(false);
+    const [scrollFromBottom, setScrollFromBottom] = useState(0);
+    const [showAttachmentPreview, setShowAttachmentPreview] = useState(false);
+    const [previewAttachment, setPreviewAttachment] = useState({});
+    const loadMoreIntersect = useRef(null);
+    const messagesCtrRef = useRef(null);
+    const { on } = useEventBus();
+
+    const messageCreated = (message) => {
+        if(selectedConversation && selectedConversation.is_group && selectedConversation.id == message.group_id) {
+            setLocalMessages((prevMessages) => [...prevMessages, message]);
+        }
+
+        if(
+            selectedConversation && selectedConversation.is_user && 
+            (selectedConversation.id == message.sender_id || selectedConversation.id == message.receiver_id)
+        ) {
+            setLocalMessages((prevMessages) => [...prevMessages, message]);
+        }
+    };
+
+    const messageDeleted = ({message}) => {
+        // console.log(message);
+        // debugger;
+        if(selectedConversation && selectedConversation.is_group && selectedConversation.id == message.group_id) {
+            setLocalMessages((prevMessages) => {
+                return prevMessages.filter((m) => m.id !== message.id);
+            });
+        }
+
+        if(
+            selectedConversation && selectedConversation.is_user && 
+            (selectedConversation.id == message.sender_id || selectedConversation.id == message.receiver_id)
+        ) {
+            setLocalMessages((prevMessages) => {
+                return prevMessages.filter((m) => m.id !== message.id);
+            });
+        }
+    };
+
+    const loadMoreMessages = useCallback(() => {
+
+        if(noMoreMessages)
+            return;
+
+        const firstMessage = localMessages[0];
+        axios
+            .get(route("message.loadOlder", firstMessage.id))
+            .then(({ data }) => {
+                if(data.data.length === 0) {
+                    setNoMoreMessages(true);
+                    return;
+                }
+
+                const scrollHeight = messagesCtrRef.current.scrollHeight;
+                const scrollTop = messagesCtrRef.current.scrollTop;
+                const clientHeight = messagesCtrRef.current.clientHeight;
+                const tmpScrollFromBottom = scrollHeight - scrollTop - clientHeight;
+
+                // console.log("TmpScrollFromBottom: ", tmpScrollFromBottom);
+                setScrollFromBottom(tmpScrollFromBottom);
+
+                setLocalMessages((prev) => {
+                    return [...data.data.reverse(), ...prev];
+                })
+            })
+    }, [localMessages, noMoreMessages]);
+
+    const onAttachmentClick = (attachments, ind) => {
+        setPreviewAttachment({
+            attachments,
+            ind,
+        });
+        setShowAttachmentPreview(true);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            if(messagesCtrRef.current) {
+                messagesCtrRef.current.scrollTop = messagesCtrRef.current.scrollHeight;
+            }
+        }, 10);
+
+        const offCreated = on('message.created', messageCreated);
+        const offDeleted = on('message.deleted', messageDeleted);
+
+        setScrollFromBottom(0);
+        setNoMoreMessages(false);
+
+        return () => {
+            offCreated();
+            offDeleted();
+        }
+    }, [selectedConversation])
+
+    useEffect(() => {
+        setLocalMessages(messages ? messages.data.reverse() : []);
+    }, [messages]);
+
+    useEffect(() => {
+        // Recover scroll position after loading old messages
+        if(messagesCtrRef.current && scrollFromBottom !== null) {
+            messagesCtrRef.current.scrollTop = 
+            messagesCtrRef.current.scrollHeight - messagesCtrRef.current.offsetHeight - scrollFromBottom;
+        }
+
+        if(noMoreMessages)
+            return;
+
+        const observer = new IntersectionObserver(
+            (entries) => entries.forEach((entry) => entry.isIntersecting && loadMoreMessages()),
+            {
+                rootMargin: "0px 0px 250px 0px",
+            }
+        );
+
+        if(loadMoreIntersect.current) {
+            setTimeout(() => {
+                observer.observe(loadMoreIntersect.current);
+            }, 100);
+        }
+
+        return () => {
+            observer.disconnect();
+        };
+    }, [localMessages])
+
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+        <>
+            {!messages && (
+                <div className='flex flex-col gap-8 justify-center text-center items-center h-full opacity-35'>
+                    <div className='text-2xl md:text-4xl p-16 text-slate-200'>
+                        Please select conversation to see messages
                     </div>
+                    <ChatBubbleLeftRightIcon className='w-32 h-32 inline-block'/>
                 </div>
-            </div>
-        </AuthenticatedLayout>
+            )}
+
+            {messages && (
+                <>
+                    <ConversationHeader selectedConversation={selectedConversation} />
+                    <div
+                        ref={messagesCtrRef}
+                        className='flex-1 overflow-y-auto p-5'
+                    >
+                        {/* Messages */}
+                        {localMessages.length === 0 && (
+                            <div className='flex justify-center items-center h-full'>
+                                <div className='text-lg text-slate-200'>
+                                    No messages found
+                                </div>
+                            </div>
+                        )}
+                        {localMessages.length > 0 && (
+                            <div className='flex-1 flex flex-col'>
+                                <div ref={loadMoreIntersect}></div>
+                                {localMessages.map((message) => (
+                                    <MessageItem 
+                                        key={message.id}
+                                        message={message}
+                                        attachmentClick={onAttachmentClick}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <MessageInput conversation={selectedConversation} />
+                </>
+            )}
+
+            {previewAttachment.attachments && (
+                <AttachmentPreviewModal 
+                    attachments={previewAttachment.attachments}
+                    index={previewAttachment.ind}
+                    show={showAttachmentPreview}
+                    onClose={() => setShowAttachmentPreview(false)}
+                />
+            )}
+        </>
     );
 }
+
+Home.layout = (page) => {
+    return (
+        <AuthenticatedLayout user={page.props.auth.user}>
+            <ChatLayout children={page} />
+        </AuthenticatedLayout>
+    )
+}
+
+export default Home;
